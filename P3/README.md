5e5cab020c1368be8f15b74ba24766afe6a2d2ba
Iago98 <iagooterogoce@gmail.com>
2020-01-31 11:29:08 +0100

    subiendo p3

--- P3/README.md
+++ P3/README.md
@@ -1,6 +1,6 @@
 # P3
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.20.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.22.
 
 ## Development server
 
--- P3/src/app/app-routing.module.ts
+++ P3/src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { WhereweareComponent } from './whereweare/whereweare.component';
+import { WhoweareComponent } from './whoweare/whoweare.component';
 import { CatalogComponent } from './catalog/catalog.component';
-
 
 const routes: Routes = [
   { path: '', redirectTo: '/homeComponent', pathMatch: 'full' },
   { path: 'catalog', component: CatalogComponent  },
+  { path: 'whoweare', component: WhoweareComponent  },
+  { path: 'whereweare', component: WhereweareComponent  },
+  { path: 'contactus', component: ContactusComponent  },
   { path: 'home', component: HomeComponent  }
-
 ];
 
 @NgModule({
